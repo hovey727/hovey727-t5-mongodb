3340c25c04465e1a7553ec14fde442ef27fb8ea7
hovey727 <hovey727@126.com>
2012-10-11 17:42:09 +0800

    mongodb operation sample ok

--- src/main/java/com/ghoome/pages/Index.java
+++ src/main/java/com/ghoome/pages/Index.java
@@ -1,6 +1,43 @@
 package com.ghoome.pages;
 
-public class Index
-{
-    
+import java.util.Arrays;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import com.ghoome.entity.Person;
+import com.ghoome.entity.Person.Sex;
+import com.ghoome.repository.PersonRepository;
+
+public class Index {
+	@Inject
+	private PersonRepository repository;
+	private Person dave, oliver, carter, boyd, stefan, leroi, alicia;
+	
+	Object onActionFromIndexForm(){
+		repository.deleteAll();
+
+		dave = new Person("Dave", "Matthews", 42);
+		oliver = new Person("Oliver August", "Matthews", 4);
+		carter = new Person("Carter", "Beauford", 49);
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		boyd = new Person("Boyd", "Tinsley", 45);
+		stefan = new Person("Stefan", "Lessard", 34);
+		leroi = new Person("Leroi", "Moore", 41);
+		alicia = new Person("Alicia", "Keys", 30, Sex.FEMALE);
+
+		repository.save(Arrays.asList(oliver, dave, carter, boyd, stefan, leroi, alicia));
+		
+		List<Person> result = repository.findAll();
+		for(Person p:result){
+			System.out.println("person: "+p.toString());
+		}
+		
+		return this;
+	}
 }
